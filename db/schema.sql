26bd02b4da4103be93f6c06f5e19329051c6bc76
Matias Martinez <matiasfmartinez27@gmail.com>
2026-05-15 14:25:43 -0300

    feat(db): actualiza tabla users con name, password_hash y created_at #main-s3-us7

--- db/schema.sql
+++ db/schema.sql
@@ -15,8 +15,10 @@ CREATE TABLE IF NOT EXISTS categories (
 
 CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
   email TEXT NOT NULL UNIQUE,
-  password TEXT NOT NULL
+  password_hash TEXT NOT NULL,
+  created_at TEXT DEFAULT (datetime('now'))
 );
 
 CREATE TABLE IF NOT EXISTS orders (
